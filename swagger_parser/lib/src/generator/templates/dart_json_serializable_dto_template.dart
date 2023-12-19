a396ad8f822e2e7fba0c30062e4e5a04b16f8442
StarProxima <maks.prozorov1@yandex.ru>
2023-12-19 11:54:18 +0300

    Revert "Fix default datetime for models"
    This reverts commit 5f109980bf97f50074e3a7aedda5c1ed299e8141.

--- swagger_parser/lib/src/generator/templates/dart_json_serializable_dto_template.dart
+++ swagger_parser/lib/src/generator/templates/dart_json_serializable_dto_template.dart
@@ -63,5 +63,5 @@ String _required(UniversalType t) =>
 String _defaultValue(UniversalType t) => t.defaultValue != null
     ? ' = '
         '${t.arrayDepth > 0 ? 'const ' : ''}'
-        '${t.enumType != null ? '${t.type}.${protectDefaultEnum(t.defaultValue)?.toCamel}' : protectDefaultValue(t.defaultValue, type: t.type, format: t.format)}'
+        '${t.enumType != null ? '${t.type}.${protectDefaultEnum(t.defaultValue)?.toCamel}' : protectDefaultValue(t.defaultValue, type: t.type)}'
     : '';
