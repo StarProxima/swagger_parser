b664022e9bde361cf00a155bd03744b7c460e3b9
StarProxima <34741787+StarProxima@users.noreply.github.com>
2024-11-29 21:26:43 +0300

    Unfix

--- swagger_parser/lib/src/generator/models/universal_type.dart
+++ swagger_parser/lib/src/generator/models/universal_type.dart
@@ -162,11 +162,10 @@ extension UniversalTypeX on UniversalType {
   }
 
   String _questionMark(ProgrammingLanguage lang) {
-    final questionMark = isRequired && !nullable ||
-            arrayDepth > 0 ||
-            (defaultValue != null && !nullable)
-        ? ''
-        : '?';
+    final questionMark =
+        isRequired && !nullable || arrayDepth > 0 || (defaultValue != null)
+            ? ''
+            : '?';
     switch (lang) {
       case ProgrammingLanguage.dart:
         return type.toDartType(format) + questionMark;
